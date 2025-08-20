ddaa88a3a0a2485ee442f8628d98ead25d66cff1
Perry Huang <perryhuang2015@gmail.com>
2025-08-20 14:50:38 -0400

    feat: add weird routing

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 import { createServer } from "node:http";
 import { URL } from "node:url";
+import * as fs from "node:fs/promises";
 
 const hostname = "127.0.0.1";
 const port = "8080";
 
-const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Mow");
+const server = createServer(async (req, res) => {
+    const theUrl = new URL(`http://${hostname}:${req.url}`);
+    if (theUrl.pathname === "/") {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        const html = await fs.readFile("./index.html");
+        res.end(html);
+    } else if (theUrl.pathname === "/contact-me") {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        const html = await fs.readFile(`.${theUrl.pathname}.html`);
+        res.end(html);
+    } else if (theUrl.pathname === "/about") {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        const html = await fs.readFile(`.${theUrl.pathname}.html`);
+        res.end(html);
+    } else {
+        res.statusCode = 404;
+        res.setHeader("Content-Type", "text/html");
+        const html = await fs.readFile("./404.html");
+        res.end(html);
+    }
 });
 
 server.listen(port, hostname, () => {
