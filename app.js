65978ce7b188e96daa827a6f07c09a25caa00e5f
Perry Huang <perryhuang2015@gmail.com>
2025-08-20 18:11:13 -0400

    feat: use express for routing

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+app.get("/contact-me", (req, res) => res.send(__dirname + "/contact-me.html"));
+app.get("/about", (req, res) => res.send(__dirname + "/about.html"));
+app.all("/*splat", (req, res) => res.sendFile(__dirname + "/404.html"));
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
